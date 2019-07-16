72a1145e20f86e5d1d5202d9f2f1d8afefbcdd1e
Jugal Kukreja <52689483+meta-jugal-kukreja@users.noreply.github.com>
2019-07-16 16:23:13 +0530

    Changed the file as said

--- SCF-Assignment-2/src/Question1/HexCalc.java
+++ SCF-Assignment-2/src/Question1/HexCalc.java
@@ -1,33 +1,51 @@
-package Question1;
+package question1;
 
 import java.util.Scanner;
 
 public class HexCalc {
+	private final String HEXADECIMAL_DIGITS = "0123456789ABCDEF"; // This string
+																	// contains
+																	// all the
+																	// hexadecimal
+																	// characters
 
 	/**
 	 * Converts Hexadecimal number to decimal number.
 	 * 
 	 * @param hexadecimalNumber
 	 *            is the hexadecimal number string.
-	 * @return integer decimal number
+	 * @return {int} decimal number
 	 */
 	public int convertHexadecimalNumberToDecimalNumber(String hexadecimalNumber) {
 
-		String allHexadecimalNumbers = "0123456789ABCDEF";
 		hexadecimalNumber = hexadecimalNumber.toUpperCase();
-		int DecimalNumber = 0;
-
+		int decimalNumber = 0;
 		for (int i = 0; i < hexadecimalNumber.length(); i++) {
 			char character = hexadecimalNumber.charAt(i);
-			if(allHexadecimalNumbers.indexOf(character) < 0)
-			{
-				DecimalNumber = -1;
-				break;
-			}
-			int number = allHexadecimalNumbers.indexOf(character);
-			DecimalNumber = 16 * DecimalNumber + number;
+			int number = HEXADECIMAL_DIGITS.indexOf(character);
+			decimalNumber = 16 * decimalNumber + number;
 		}
-		return DecimalNumber;
+		return decimalNumber;
+	}
+
+	/**
+	 * This method checks whether the hexadecimal string is a valid is a valid
+	 * hexadecimal number
+	 * 
+	 * @param hexadecimalNumber
+	 *            is the hexadecimal string to be validated
+	 * @return {boolean}true if the hexadecimal string is valid hexadecimal
+	 *         number else false
+	 */
+	public boolean hexadecimalNumberValidation(String hexadecimalNumber) {
+		hexadecimalNumber = hexadecimalNumber.toUpperCase();
+		for (int i = 0; i < hexadecimalNumber.length(); i++) {
+			char character = hexadecimalNumber.charAt(i);
+			if (HEXADECIMAL_DIGITS.indexOf(character) < 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -35,18 +53,18 @@ public class HexCalc {
 	 * 
 	 * @param decimalNumber
 	 *            is the integer decimal number
-	 * @return integer hexadecimal number string
+	 * @return {String} hexadecimal number string
 	 */
 	public String convertDecimalNumberToHexadecimalNumber(int decimalNumber) {
 		int intermediateNumber;
 		String hexadecimalNumber = "";
-		String allHexadecimalNumbers = "0123456789ABCDEF";
+		String HEXADECIMAL_DIGITS = "0123456789ABCDEF";
 
 		if (decimalNumber != 0) {
 			while (decimalNumber > 0) {
 
 				intermediateNumber = decimalNumber % 16;
-				hexadecimalNumber = allHexadecimalNumbers
+				hexadecimalNumber = HEXADECIMAL_DIGITS
 						.charAt(intermediateNumber) + hexadecimalNumber;
 				decimalNumber = decimalNumber / 16;
 			}
@@ -62,6 +80,7 @@ public class HexCalc {
 	public void performArithematicOpertaionsOnTwoHexadecimalNumbers() {
 		Scanner inputScanner = new Scanner(System.in);
 		String number1, number2;
+		int choice = 0;
 
 		System.out.println("What do you want to do? ");
 		System.out.println("1. Add two Hexadecimal numbers.");
@@ -71,7 +90,12 @@ public class HexCalc {
 		System.out.println("5. Return to previous menu.");
 
 		System.out.print("Enter your Choice : ");
-		int choice = inputScanner.nextInt();
+		try {
+			choice = inputScanner.nextInt();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(0);
+		}
 
 		System.out.print("\nEnter Hexadecimal Number 1 : ");
 		number1 = inputScanner.next();
@@ -123,37 +147,18 @@ public class HexCalc {
 	public void compareTwoHexadecimalNumbers() {
 		Scanner inputScanner = new Scanner(System.in);
 		String number1, number2;
-		System.out.println("What do you want to do? ");
-		System.out.println("1. Check equality of two Hexadecimal numbers.");
-		System.out
-				.println("2. Check whether first hexadecimal number is greater than second hexadecimal number.");
-		System.out
-				.println("3. Check whether first hexadecimal number is smaller than second hexadecimal number.");
-		System.out.println("4. Return to previous menu.");
-
-		System.out.print("Enter your Choice : ");
-		int choice = inputScanner.nextInt();
 
 		System.out.print("\nEnter Hexadecimal Number 1 : ");
 		number1 = inputScanner.next();
 		System.out.print("Enter Hexadecimal Number 2 : ");
 		number2 = inputScanner.next();
 
-		switch (choice) {
-		case 1:
-			System.out.println(checkEqualityOfTwoHexadecimalNumbers(number1,
-					number2));
-			break;
-		case 2:
-			System.out.println(isFirstGreater(number1, number2));
-			break;
-		case 3:
-			System.out.println(isFirstSmaller(number1, number2));
-			break;
-		case 4:
-			return;
-		default:
-		}
+		if (checkEqualityOfTwoHexadecimalNumbers(number1, number2))
+			System.out.println("Two numbers are equal");
+		else if (isFirstGreater(number1, number2))
+			System.out.println("First Number is greater");
+		else if (isFirstSmaller(number1, number2))
+			System.out.println("Second number is greater");
 	}
 
 	/**
@@ -163,7 +168,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return addition of two hexadecimal numbers string
+	 * @return {String}addition of two hexadecimal numbers
 	 */
 	public String addTwoHexadecimalNumbers(String number1, String number2) {
 		int decimalNumber1 = convertHexadecimalNumberToDecimalNumber(number1);
@@ -179,7 +184,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return subtraction of two hexadecimal numbers string
+	 * @return {String}subtraction of two hexadecimal numbers
 	 */
 	public String subtractTwoHexadecimalNumbers(String number1, String number2) {
 		int decimalNumber1 = convertHexadecimalNumberToDecimalNumber(number1);
@@ -196,7 +201,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return multiplication of two hexadecimal numbers string
+	 * @return {String}multiplication of two hexadecimal numbers
 	 */
 	public String multiplyTwoHexadecimalNumbers(String number1, String number2) {
 		int decimalNumber1 = convertHexadecimalNumberToDecimalNumber(number1);
@@ -212,7 +217,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return Quotient and Remainder of two hexadecimal numbers string
+	 * @return {String}Quotient and Remainder of two hexadecimal numbers
 	 */
 	public String divideTwoHexadecimalNumbers(String number1, String number2) {
 		int decimalNumber1 = convertHexadecimalNumberToDecimalNumber(number1);
@@ -247,7 +252,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return true if equal else false
+	 * @return {boolean}true if equal else false
 	 */
 	public boolean isFirstGreater(String number1, String number2) {
 		boolean answer = false;
@@ -257,9 +262,14 @@ public class HexCalc {
 			answer = false;
 		else {
 			for (int i = 0; i < number1.length(); i++) {
-				if (number1.charAt(i) > number2.charAt(i)) {
-					answer = true;
-					break;
+				if (number1.charAt(i) != number2.charAt(i)) {
+					if (number1.charAt(i) > number2.charAt(i)) {
+						answer = true;
+						break;
+					} else {
+						answer = false;
+						break;
+					}
 				}
 			}
 		}
@@ -273,7 +283,7 @@ public class HexCalc {
 	 *            is the first hexadecimal number string
 	 * @param number2
 	 *            is the second hexadecimal number string
-	 * @return true if equal else false
+	 * @return {boolean}true if equal else false
 	 */
 	public boolean isFirstSmaller(String number1, String number2) {
 		boolean answer = false;
@@ -283,9 +293,14 @@ public class HexCalc {
 			answer = false;
 		else {
 			for (int i = 0; i < number1.length(); i++) {
-				if (number1.charAt(i) < number2.charAt(i)) {
-					answer = true;
-					break;
+				if (number1.charAt(i) != number2.charAt(i)) {
+					if (number1.charAt(i) < number2.charAt(i)) {
+						answer = true;
+						break;
+					} else {
+						answer = false;
+						break;
+					}
 				}
 			}
 		}

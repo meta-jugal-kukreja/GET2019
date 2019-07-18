bf62efb3a0d0ae8ff9ee3321feb0b3f37f56014a
jugal kukreja <jugal.kukreja@metacube.com>
2019-07-18 19:28:44 +0530

    Commit for submission

--- SCF-Session-5-Assignment/src/question1/NumberOperationsTest.java
+++ SCF-Session-5-Assignment/src/question1/NumberOperationsTest.java
@@ -11,41 +11,75 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 /**
- * This class is used to test the HCF of two numbers
+ * This class is used to test the NumberOperations class
  * @author Jugal
  *
  */
-@RunWith(Parameterized.class)
-public class NumberOperationsHCFTest {
+public class NumberOperationsTest {
 	
-	private int x;
-	private int y;
-	private int expectedResult;
 	private NumberOperations numberObject;
 
+	/**
+	 * This method creates object before the tests begin
+	 */
 	@Before
 	public void initialize() {
 		numberObject = new NumberOperations();
 	}
 
-	public NumberOperationsHCFTest(int expectedResult, int x, int y) {
-		this.expectedResult = expectedResult;
-		this.expectedResult = x;
-		this.y = y;
-	}
-
-	@Parameterized.Parameters
-	public static Collection data() {
-		return Arrays.asList(new Object[][] {
-				{90, 180, 270},
-				{12 ,300, 396},
-				{14, 98, 56}
-				});
-	}
-
+	/**
+	 * These tests are performed on HCF
+	 */
 	@Test
-	public void clumpTest() {
-		assertEquals(expectedResult, numberObject.computeHCF(x, y));
+	public void testHCF1() {
+		int x = 180;
+		int y = 270;
+		assertEquals(90, numberObject.computeHCF(x, y));
 	}
-
+	@Test
+	public void testHCF2() {
+		int x = 300;
+		int y = 396;
+		assertEquals(12, numberObject.computeHCF(x, y));
+	}
+	@Test
+	public void testHCF3() {
+		int x = 98;
+		int y = 56;
+		assertEquals(14, numberObject.computeHCF(x, y));
+	}
+	@Test(expected=AssertionError.class) //Negative test case
+	public void testHCF4() {
+		int x = 98;
+		int y = 0;
+		assertEquals(0, numberObject.computeHCF(x, y));
+	}
+	
+	/**
+	 * These tests are performed on LCM
+	 */
+	@Test
+	public void testLCM1() {
+		int x = 18;
+		int y = 24;
+		assertEquals(72, numberObject.computeLCM(x, y));
+	}
+	@Test
+	public void testLCM2() {
+		int x = 32;
+		int y = 60;
+		assertEquals(480, numberObject.computeLCM(x, y));
+	}
+	@Test
+	public void testLCM3() {
+		int x = 33;
+		int y = 30;
+		assertEquals(330, numberObject.computeLCM(x, y));
+	}
+	@Test(expected=ArithmeticException.class) //negative test case
+	public void testLCM4() {
+		int x = 0;
+		int y = 24;
+		assertEquals(72, numberObject.computeLCM(x, y));
+	}
 }

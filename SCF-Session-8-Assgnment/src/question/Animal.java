2fb90e75e0f9d100febc2861374fa17d98f71a5e
jugal kukreja <jugal.kukreja@metacube.com>
2019-07-22 19:47:09 +0530

    Just Started

---
+++ SCF-Session-8-Assgnment/src/question/Animal.java
@@ -0,0 +1,15 @@
+package question;
+
+/**
+ * This class contains members which represents some general characteristics of an animal such category, name, etc
+ * @author Jugal
+ * Dated 07/22/2019
+ */
+public class Animal {
+	
+	private String animalName;
+	private static int id = 0;
+	private String Category;
+	private double weight;
+
+}
---
+++ SCF-Session-8-Assgnment/src/question/Peacock.java
@@ -0,0 +1,5 @@
+package question;
+
+public class Peacock extends Bird{
+
+}

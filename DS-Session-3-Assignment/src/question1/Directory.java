58376a9f6c01f84644e37838264a2339711153fc
Jugal Kukreja <jugal.kukreja@metacube.com>
2019-08-04 18:56:03 +0530

    DS-Session-3-Assignment Commit for Submission

--- DS-Session-3-Assignment/src/question1/Directory.java
+++ DS-Session-3-Assignment/src/question1/Directory.java
@@ -5,40 +5,59 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Directory class storing information about a directory
- * 
+ * Directory class storing information about a directory such as Directory name, 
+   date of creation and the list of sub-directories
  * @author admin
- *
+ * Dated 08/04/2019
  */
 public class Directory {
 
-	private  String name;
-	private final Date dateOfCreation;
+	private String directoryName;
+	private Date dateOfCreation;
+	private Directory parentDirectory;
 	private List<Directory> listOfSubDirectory;
 
-	public Directory(String name, Date dateOfCreation) {
+	/**
+	 * Constructor to initialize the data members of the class
+	 * @param directoryName is the name of the directory
+	 * @param dateOfCreation is the date at which the directory is created
+	 */
+	public Directory(String directoryName, Date dateOfCreation, Directory parentDirectory) {
 		this.dateOfCreation = dateOfCreation;
-		this.name = name;
+		this.directoryName = directoryName;
+		this.parentDirectory = parentDirectory;
 		this.listOfSubDirectory = new ArrayList<Directory>();
 	}
 
+	public Directory getParentDirectory() {
+		return parentDirectory;
+	}
+
+	/**
+	 * This method returns the list of sub-directories
+	 * @return {List<Directory>}
+	 */
 	public List<Directory> getListOfSubDirectory() {
 		return listOfSubDirectory;
 	}
 
+	
 	public void setListOfSubDirectory(List<Directory> listOfSubDirectory) {
 		this.listOfSubDirectory = listOfSubDirectory;
 	}
 
+	/**
+	 * This method returns the directory name
+	 * @return {String}
+	 */
 	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name= name;
-	}
-	
+		return directoryName;
+	}	
 
+	/**
+	 * This method returns the date at which the directory was created
+	 * @return {Date}
+	 */
 	public Date getDateOfCreation() {
 		return dateOfCreation;
 	}
---
+++ DS-Session-3-Assignment/src/question1/Main.java
@@ -0,0 +1,11 @@
+package question1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		VirtualCommandPrompt v = new VirtualCommandPrompt();
+        v.virtualCommandPrompt();
+	}
+
+}

eb00b2d800e05cd3257ccfd34605f07d1efc3940
Jugal Kukreja <jugal.kukreja@metacube.com>
2019-10-20 20:08:28 +0530

    Fixed Review Comments

--- DBMS-Session-2-Assignment/question2/InsertionFile.sql
+++ DBMS-Session-2-Assignment/question2/InsertionFile.sql
@@ -1,4 +1,4 @@
-INSERT INTO user VALUES
+INSERT INTO Users(email_id, user_name, phone_no, date_of_birth, type_of_user) VALUES
 	('abc98@gmail.com', 'abc', '8947804883', '1997-10-25', 'A'),
 	('rs98@gmail.com', 'rs', '8947804889',  '1998-10-25', 'S'),
 	('def@gmail.com', 'def', '7737596610', '1997-03-10', 'S'),
@@ -42,19 +42,41 @@ INSERT INTO Products(product_name, product_price, category_id, issue_date) VALUE
     ('Van Heusen Trouser', 2000.00, 20, '2016/07/01'),
     ('Peter England Suit', 10000.00, 21, '2014/02/12');
     
-INSERT INTO Orders(order_status, order_amount, number_of_products, order_date, email_id, product_id) VALUES
-    ('Shipped', 200.00, 2, '2019-08-13', 'pqr@gmail.com', 1),
-    ('Not Shipped', 300.00, 1, '2019-06-13', 'rs98@gmail.com', 2),
-    ('Cancelled', 400.00, 3, '2018-08-15', 'def@gmail.com', 3),
-    ('Shipped', 500.00, 9, '2015-12-30', 'pqr@gmail.com', 4), 
-    ('Not Shipped', 6000.00, 7, '2019-07-13', 'xyz@gmail.com', 5),
-    ('Not Shipped', 60.00, 2, '2019-07-20', 'xyz@gmail.com', 6),
-    ('Not Shipped', 1800.00, 9, '2019-07-31', 'xyz@gmail.com', 7),
-    ('Shipped', 630.00, 2, '2019-07-20', 'xyz@gmail.com', 6),
-    ('Not Shipped', 850.00, 2, '2019-07-20', 'xyz@gmail.com', 6),
-    ('Shipped', 8000.00, 2, '2019-07-20', 'xyz@gmail.com', 6);
+INSERT INTO Orders(order_status, order_amount, order_date, user_id, product_id) VALUES
+    ('Shipped', 200.00, '2019-08-13', 5, 1),
+    ('Not Shipped', 300.00,  '2019-06-13', 2, 2),
+    ('Cancelled', 400.00,  '2018-08-15', 3, 3),
+    ('Shipped', 500.00,  '2015-12-30', 5, 4), 
+    ('Not Shipped', 6000.00,  '2019-07-13', 4, 5),
+    ('Not Shipped', 60.00,  '2019-07-20', 4, 6),
+    ('Not Shipped', 1800.00,  '2019-07-31', 4, 7),
+    ('Shipped', 630.00,  '2019-07-20', 4, 6),
+    ('Not Shipped', 850.00,  '2019-07-20', 4, 6),
+    ('Shipped', 8000.00,  '2019-07-20', 4, 6);
     
-INSERT INTO images VALUES
+INSERT INTO Cart(order_id, product_id, quantity, amount, order_status) VALUES
+	(1, 1, 2, (SELECT product_price FROM Products WHERE product_id=1)*2, 'Shipped'),
+    (1, 3, 3, (SELECT product_price FROM Products WHERE product_id=3)*3, 'Not Shipped'),
+    (2, 5, 4, (SELECT product_price FROM Products WHERE product_id=5)*4, 'Cancelled'),
+    (2, 2, 5, (SELECT product_price FROM Products WHERE product_id=2)*5, 'Shipped'),
+    (3, 8, 6, (SELECT product_price FROM Products WHERE product_id=8)*6, 'Shipped'),
+    (3, 9, 3, (SELECT product_price FROM Products WHERE product_id=9)*3, 'Not Shipped'),
+    (4, 10, 2, (SELECT product_price FROM Products WHERE product_id=10)*2, 'Cancelled'),
+    (4, 4, 4, (SELECT product_price FROM Products WHERE product_id=4)*4, 'Shipped'),
+    (5, 5, 1, (SELECT product_price FROM Products WHERE product_id=5)*1, 'Not Shipped'),
+    (5, 12, 2, (SELECT product_price FROM Products WHERE product_id=12)*2, 'Not Shipped'),
+    (6, 4, 3, (SELECT product_price FROM Products WHERE product_id=4)*3, 'Shipped'),
+    (6, 2, 4, (SELECT product_price FROM Products WHERE product_id=2)*4, 'Not Shipped'),
+    (7, 1, 6, (SELECT product_price FROM Products WHERE product_id=1)*6, 'Shipped'),
+    (8, 6, 2, (SELECT product_price FROM Products WHERE product_id=6)*2, 'Cancelled'),
+    (9, 10, 5, (SELECT product_price FROM Products WHERE product_id=10)*5, 'Shipped'),
+    (10, 12, 1, (SELECT product_price FROM Products WHERE product_id=12)*1, 'Not Shipped'),
+    (7, 2, 6, (SELECT product_price FROM Products WHERE product_id=2)*6, 'Shipped'),
+    (8, 7, 1, (SELECT product_price FROM Products WHERE product_id=7)*1, 'Cancelled'),
+    (9, 8, 2, (SELECT product_price FROM Products WHERE product_id=8)*2, 'Not Shipped');
+    
+    
+INSERT INTO Images VALUES
     (1, 'Redmi3sPrime.jpg'),
     (1, 'Redmi3sPrime1.jpg'),
     (1, 'Redmi3sPrime2.jpg'),
@@ -73,15 +95,15 @@ INSERT INTO images VALUES
     (10, 'VanHeusenShirt1.jpg'),
     (12, 'PeterEnglandSuit.jpg');
     
-INSERT INTO Shipping_Address(area, city, state, country, email_id, pincode) VALUES 
-    ('Kumbha Marg', 'Jaipur', 'Rajasthan', 'India', 'abc98@gmail.com', '302033'),
-    ('MBM Hostel', 'Jodhpur', 'Rajasthan', 'India', 'rs98@gmail.com','342204'),
-    ('Vaishali Nagar', 'Jaipur', 'Rajasthan', 'India', 'def@gmail.com','302033'),
-    ('Haldi Ghati Marg', 'Jaipur', 'Rajasthan', 'India', 'def@gmail.com','302033'),
-    ('Shakti Nagar', 'Udaipur', 'Rajasthan', 'India', 'abc98@gmail.com','313001'),
-    ('Ram Marg', 'Jaisalmair', 'Rajasthan', 'India', 'pqr@gmail.com','345001'),
-    ('Malviya Nagar', 'Jaipur', 'Rajasthan', 'India', 'xyz@gmail.com','302033'),
-    ('Bapu Nagar', 'Jaipur', 'Rajasthan', 'India', 'abc98@gmail.com','302033');
+INSERT INTO ShippingAddress(area, city, state, country, user_id, pincode) VALUES 
+    ('Kumbha Marg', 'Jaipur', 'Rajasthan', 'India', 1, '302033'),
+    ('MBM Hostel', 'Jodhpur', 'Rajasthan', 'India', 2,'342204'),
+    ('Vaishali Nagar', 'Jaipur', 'Rajasthan', 'India', 3,'302033'),
+    ('Haldi Ghati Marg', 'Jaipur', 'Rajasthan', 'India', 3,'302033'),
+    ('Shakti Nagar', 'Udaipur', 'Rajasthan', 'India', 2,'313001'),
+    ('Ram Marg', 'Jaisalmair', 'Rajasthan', 'India', 5,'345001'),
+    ('Malviya Nagar', 'Jaipur', 'Rajasthan', 'India', 4,'302033'),
+    ('Bapu Nagar', 'Jaipur', 'Rajasthan', 'India', 3,'302033');
     
 
 INSERT INTO Stock (product_id, amount_of_product_in_stock) VALUES 
@@ -97,4 +119,6 @@ INSERT INTO Stock (product_id, amount_of_product_in_stock) VALUES
     (10,50),
     (11,50),
     (12,20);
+    
+
     
